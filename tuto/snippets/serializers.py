5a18d5b31bc704e7d61f4333a116d939ae07c4ba
Tomas Martin <martintomasalejandro@gmail.com>
2024-07-25 10:34:43 -0300

    feat: finish up 5th tutorial class

---
+++ tuto/snippets/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from snippets.models import Snippet
+from django.contrib.auth.models import User
+
+
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        owner = serializers.ReadOnlyField(source='owner.username')
+        highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+        fields = ['url', 'id', 'highlight', 'owner',
+                  'title', 'code', 'linenos', 'language', 'style']
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['url', 'id', 'username', 'snippets']
